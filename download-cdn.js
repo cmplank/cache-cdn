ec949b72bc04faf15e5ac74c79711bbec78479be
Colin Plank <colinmplank@gmail.com>
2018-03-11 17:16:48 -0400

    Add feature: Always download cdn entries

--- download-cdn.js
+++ download-cdn.js
@@ -1,28 +1,87 @@
 'use strict';
 
-const fs = require('fs');
+const Promise = require("bluebird");
+const fs = Promise.promisifyAll(require("fs"));
+const mkdirp = Promise.promisify(require("mkdirp"));
+const downloadFile = Promise.promisify(require("download-file"));
 
 const downloadCdn = (options) => {
+	validateOptions(options);
+	let filesToProcess = options.filesToProcess;
+	let downloadLibs = options.downloadLibs;
+
+	// Lookup config in cdn.json
+	let mainPromise = fs.readFileAsync("cdn.json", 'utf8').then((data) => {
+		let config = JSON.parse(data);
+		config = parseDependencies(config);
+		let blockPromises = [];
+
+		// Iterate through config blocks
+		Object.values(config).forEach(function (block) {
+
+			// Download Libs
+			if (downloadLibs) {
+				blockPromises.push(alwaysDownloadCdnLibs(block));
+			}
+
+			// Process Html File
+			if (filesToProcess) {
+				blockPromises.push(replaceStringWithCdnEntries(block, filesToProcess));
+			}
+		});
+
+		return Promise.all(blockPromises);
+	});
+
+	return mainPromise;
+};
+
+function validateOptions(options) {
 	if (!options || Object.keys(options).length === 0) {
 		throw new Error("No options provided, so do nothing");
 	}
 
-	let filesToProcess = options.filesToProcess;
-	if (filesToProcess && !Array.isArray(filesToProcess)) {
+	if (options.filesToProcess && !Array.isArray(options.filesToProcess)) {
 		throw new Error("option.filesToProcess must be an array");
 	}
 
-	let downloadLibs = options.downloadLibs;
-	if (downloadLibs && typeof downloadLibs !== 'boolean') {
+	if (options.downloadLibs && typeof options.downloadLibs !== 'boolean') {
 		throw new Error("option.downloadLibs must be a boolean");
 	}
+}
 
-	// Iterate through blocks
-	//   Download Libs if needed
-	//   Process Files if needed
+function parseDependencies(config) {
+	Object.keys(config).forEach(function (blockName) {
+		let block = config[blockName];
+		block.dependencies = block.dependencies.map((url) => {
+			return {
+				url: url,
+				filename: url.substring(url.lastIndexOf('/') + 1)
+			};
+		});
+	});
 
-	// todo: Add cdn-lock.json and use to ensure fresh downloads when lib versions change
+	return config;
+}
 
-};
+// TODO: Only download libs as needed
+function alwaysDownloadCdnLibs(block) {
+	// Create folder if none exists
+	return mkdirp(block.downloadDirectory).then(() => {
+		// Download files from cdn into it
+		let filePromises = block.dependencies.map(function (dependency) {
+			return downloadFile(dependency.url, {
+				directory: block.downloadDirectory,
+				filename: dependency.filename
+			});
+		});
+
+		return Promise.all(filePromises);
+	});
+}
+
+function replaceStringWithCdnEntries(block, filesToProcess) {
+	// Replace "replaceString" with files in format "replaceFormat"
+}
 
 module.exports = downloadCdn;
--- test/spec.js
+++ test/spec.js
@@ -1,7 +1,21 @@
-var expect = require("chai").expect;
-var downloadCdn = require("../download-cdn");
+
+let expect = require("chai").expect;
+let downloadCdn = require("../download-cdn");
+
+let Promise = require("bluebird");
+let rimraf = Promise.promisify(require("rimraf"));
+let fs = Promise.promisifyAll(require("fs"));
 
 describe("download cdn", function () {
+
+    let jsFile = "tmp/js/jquery.min.js";
+    let cssFile = "tmp/css/bootstrap.min.css";
+
+    beforeEach(function () {
+        // delete tmp directory
+        return rimraf("tmp");
+    });
+
     it("fails when no options passed", function () {
         let noOptionsCall = () => downloadCdn();
         expect(noOptionsCall).to.throw();
@@ -14,13 +28,13 @@ describe("download cdn", function () {
     });
 
     it("fails when options.filesToProcess is not an array", function () {
-        let options = {filesToProcess: "file.txt"};
+        let options = { filesToProcess: "file.txt" };
         let normalCall = () => downloadCdn(options);
         expect(normalCall).to.throw();
     });
 
     it("fails when options.downloadLibs is not a boolean", function () {
-        let options = {downloadLibs: ["my/directory"]};
+        let options = { downloadLibs: ["my/directory"] };
         let normalCall = () => downloadCdn(options);
         expect(normalCall).to.throw();
     });
@@ -30,8 +44,13 @@ describe("download cdn", function () {
             downloadLibs: true,
             filesToProcess: ["app/index.html", "moreFilez"]
         };
-        let normalCall = () => downloadCdn(options);
-        expect(normalCall).not.to.throw();
-        expect(2 + 2).to.equal(4);
+        return promise = downloadCdn(options).then(function () {
+            let jsPromise = fs.accessAsync(jsFile);
+            let cssPromise = fs.accessAsync(cssFile);
+
+            expect(2 + 2).to.equal(4);
+
+            return Promise.all([jsPromise, cssPromise]);
+        });
     });
 });
